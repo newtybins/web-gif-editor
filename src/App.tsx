9549892b289325a48f7499443405f0e04f7259c2
newt! <newtykins@outlook.com>
2021-11-07 22:48:03 +0000

    Load in ffmpeg

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [ready, setReady] = useState(false);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  // Load ffmpeg as the component is mounted
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    load();
+  }, []);
+
+  return ready ? <div></div> : <p>Loading...</p>;
 }
 
 export default App;
